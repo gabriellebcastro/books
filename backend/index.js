0624f951685a47b01536f04269ac4ffe9ec36e37
gabriellebcastro <gabriellebcastro@gmail.com>
2025-10-30 08:58:44 -0300

    Configurando autenticação

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
-// import userRoutes from './routes/userRoutes.js';
-// import bookRoutes from './routes/bookRoutes.js';
-// import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
+import bookRoutes from './routes/bookRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -19,11 +19,11 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-// app.use('/api/users', userRoutes);
-// app.use('/api/books', bookRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/books', bookRoutes);
 
-// app.use(notFound);
-// app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
